e2c6899efc232334003d806f11ed563f634d31fe
Andrey <andreybicalho@gmail.com>
2018-05-04 23:46:36 -0300

    Abstracting OpenGL into Classes

---
+++ OpenGL/src/IndexBuffer.cpp
@@ -0,0 +1,26 @@
+#include "IndexBuffer.h"
+#include "Renderer.h"
+
+IndexBuffer::IndexBuffer(const unsigned int* data, unsigned int count) : Count(count)
+{
+	ASSERT(sizeof(unsigned int ) == sizeof(GLuint));
+
+	GLCall(glGenBuffers(1, &RendererID));
+	GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, RendererID));
+	GLCall(glBufferData(GL_ELEMENT_ARRAY_BUFFER, count * sizeof(unsigned int), data, GL_STATIC_DRAW));
+}
+
+IndexBuffer::~IndexBuffer()
+{
+	GLCall(glDeleteBuffers(1, &RendererID));
+}
+
+void IndexBuffer::Bind() const
+{
+	GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, RendererID));
+}
+
+void IndexBuffer::Unbind() const
+{
+	GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0));
+}
