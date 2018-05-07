8d38b774a56a9f7ea5504a12e0f952d8584b2a45
Andrey <andreybicalho@gmail.com>
2018-05-06 23:57:35 -0300

    Shader Abstraction in OpenGL

---
+++ OpenGL/src/Shader.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <string>
+
+struct ShaderProgramSource
+{
+	std::string VertexSource;
+	std::string FragmentSource;
+};
+
+class Shader
+{
+private:
+	std::string FilePath;
+	unsigned int RendererID;
+public:
+	Shader(const std::string& filepath);
+	~Shader();
+
+	void Bind() const;
+	void Unbind() const;
+
+	void SetUniform4f(const std::string& name, float v0, float v1, float v2, float v3);
+
+private:
+	ShaderProgramSource ParseShader(const std::string& filepath);
+	unsigned int CreateShader(const std::string& vertexShader, const std::string& fragmentShader);
+	unsigned int CompileShader(unsigned int type, const std::string& source);
+	int GetUniformLocation(const std::string& name);
+
+};
