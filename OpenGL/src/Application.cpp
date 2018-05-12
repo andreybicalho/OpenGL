a07ee0ff065b7b64e46c9b6531910fee5c9a7014
Andrey <andreybicalho@gmail.com>
2018-05-12 00:24:31 -0300

    ImGui in OpenGL - Part 2

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -95,14 +95,11 @@ int main(void)
 		
 		glm::mat4 proj = glm::ortho(0.0f, 960.0f, 0.0f, 540.f, -1.0f, 1.0f); 
 		glm::mat4 view = glm::translate(glm::mat4(1.f), glm::vec3(-100, 0, 0));
-		glm::mat4 model = glm::translate(glm::mat4(1.f), glm::vec3(200, 200, 0));
-		
-		glm::mat4 mvp = proj * view * model; // multiplication must be pvm and not mpv
 		
 		Shader shader("res/shaders/Basic.shader");
 		shader.Bind();
 		shader.SetUniform4f("u_Color", 0.8f, 0.3f, 0.8f, 1.0f); // our parameter in the shader program
-		shader.SetUniformMat4f("u_MVP", mvp);
+		
 
 		Texture texture("res/textures/opengl_logo.png");
 		texture.Bind();
@@ -121,10 +118,7 @@ int main(void)
 		ImGui_ImplGlfwGL3_Init(window, true);
 		ImGui::StyleColorsDark();
 
-		bool show_demo_window = true;
-		bool show_another_window = false;
-		ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
-
+		glm::vec3 translation(200, 200, 0);
 
 
 		float r = 0.0f;
@@ -137,8 +131,12 @@ int main(void)
 
 			ImGui_ImplGlfwGL3_NewFrame();
 
+			glm::mat4 model = glm::translate(glm::mat4(1.f),translation);
+			glm::mat4 mvp = proj * view * model;
+
 			shader.Bind();
 			shader.SetUniform4f("u_Color", r, 0.3f, 0.8f, 1.0f);
+			shader.SetUniformMat4f("u_MVP", mvp);
 
 			renderer.Draw(va, ib, shader);
 
@@ -155,20 +153,7 @@ int main(void)
 
 
 			{
-				static float f = 0.0f;
-				static int counter = 0;
-				ImGui::Text("Hello, world!");                           // Display some text (you can use a format string too)
-				ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f    
-				ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
-
-				ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our windows open/close state
-				ImGui::Checkbox("Another Window", &show_another_window);
-
-				if (ImGui::Button("Button"))                            // Buttons return true when clicked (NB: most widgets return true when edited/activated)
-					counter++;
-				ImGui::SameLine();
-				ImGui::Text("counter = %d", counter);
-
+				ImGui::SliderFloat3("Translation", &translation.x, 0.0f, 960.0f);
 				ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
 			}
 
